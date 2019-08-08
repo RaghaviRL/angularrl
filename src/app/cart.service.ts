8f1a85c58fb0372682b3e556054356ed065692e5
RaghaviRL <raghavirl97@gmail.com>
2019-08-08 14:34:26 +0530

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable(
   {providedIn : 'root'}
@@ -18,6 +19,8 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  constructor(    
+    private http: HttpClient
+) { }
 
 }
